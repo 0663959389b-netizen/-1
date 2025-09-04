5e1e16718c0311a6a53d231f142ddb1c758d12fb
Bohdana Poveda <0663959389b@gmail.com>
2025-09-04 14:15:50 +0300

    Add files via upload

---
+++ лаба 1.py
@@ -0,0 +1,47 @@
+age = 17
+# створює змінну age з значенням 17
+print('age,', type(age), ':', age)
+# виводить змінну age з його типом і значенням
+height = 1.90
+# створює змінну height з значенням 1.90
+print('height,', type(height), ':', height)
+# виводить змінну height з його типом і значенням
+name = 'John'
+# створює змінну name place_of_education John
+print('name,', type(name), ':', name)
+# виводить змінну name з його типом і значенням
+is_student = True
+# створює змінну is_student з значенням True
+print('is_student,', type(is_student), ':', is_student)
+# виводить змінну is_student з його типом і значенням
+empty_list = []
+# створює змінну empty_list без значення
+print('empty_list,', type(empty_list), ':', empty_list)
+# виводить змінну empty_list з його типом і значенням
+grades = [5, 4, 5]
+# створює змінну gradesс з значенням 5, 4, 5
+print('grades,', type(grades), ':', grades)
+# виводить змінну grades з його типом і значенням
+classmates = {"Bohdana", "Andrii", "Valeria"}
+# створює змінну classmates з значенням імен одногрупників
+print('classmates,', type(classmates), ':', classmates)
+# виводить змінну classmates з його типом і значенням
+number_of_students = (2, 1, 3)
+# створює змінну number_of_students з значенням їх номеру
+print('number_of_students,', type(number_of_students), ':', number_of_students)
+# виводить змінну number_of_students з його типом і значенням
+place_of_education = {1:"Ukraine", 2:"Lutsk", 3:"Konyakina str."}
+# створює змінну place_of_education з значенням адреси коледжу
+print('place_of_education,', type(place_of_education), ':', place_of_education)
+# виводить змінну place_of_education з його типом і значенням
+
+a=20
+b=8
+print('a+b=',a+b) #додавання
+print('a-b=',a-b) #віднімання
+print('a*b=',a*b) #множення
+print('a/b=',a/b) #ділення
+print('a%b=',a%b) #остача
+print('a**b=',a**b) #степінь
+print('a//b=',a//b) #ділення без остачі
+
