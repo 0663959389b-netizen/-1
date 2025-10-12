34a3a8bd3e0ba3c23bcc808a43b9fec7449a7693
Bohdana Poveda <0663959389b@gmail.com>
2025-10-12 18:35:59 +0300

    Delete Lab6/LAB6 directory

--- Lab6/LAB6/decorator.py
+++
@@ -1,9 +0,0 @@
-def log_exceptions(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            with open("errors.log", "a", encoding="utf-8") as f:
-                f.write(f"Помилка у функції {func.__name__}: {e}\n")
-            raise  # піднімаємо помилку далі
-    return wrapper
