610f959e8602195abb0ed19d06c0d81d2cf59216
Bohdana Poveda <0663959389b@gmail.com>
2025-10-12 18:29:18 +0300

    Update decorator.py

--- Lab6/decorator.py
+++ Lab6/decorator.py
@@ -1,10 +1,2 @@
-def log_exceptions(func):
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except Exception as e:
-            with open("errors.log", "a", encoding="utf-8") as f:
-                f.write(f"Помилка у функції {func.__name__}: {e}\n")
-            raise  # піднімаємо помилку далі
-    return wrapper
+Видалено зайвий файл
 
