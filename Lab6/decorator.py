c7f72155b5fdd32b9d9319160fee6e74539a7df6
Bohdana Poveda <0663959389b@gmail.com>
2025-10-12 18:17:58 +0300

    decorator.py

--- Lab6/decorator.py
+++ Lab6/decorator.py
@@ -1,13 +1,10 @@
-import traceback
-
 def log_exceptions(func):
     def wrapper(*args, **kwargs):
         try:
-            return func(*args, **kwargs)  # спробувати виконати оригінальну функцію
+            return func(*args, **kwargs)
         except Exception as e:
-            with open("errors.log", "a", encoding="utf-8") as f:  # відкрити файл у режимі додавання
-                f.write(f"Exception in function '{func.__name__}': {e}\n")
-                f.write(traceback.format_exc())  # записати повний трейсбек помилки
-                f.write("\n" + "-"*40 + "\n")
-            raise  # пробросити помилку далі, якщо треба
+            with open("errors.log", "a", encoding="utf-8") as f:
+                f.write(f"Помилка у функції {func.__name__}: {e}\n")
+            raise  # піднімаємо помилку далі
     return wrapper
+
